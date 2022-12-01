fd5587b663996aba959c18451a6fa5c22faae906
Leonardo Danelutti <leodane2002@gmail.com>
2022-12-01 13:26:07 +0100

    In_oreder procedure in es18.

--- esercizio18/main.c
+++ esercizio18/main.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
-int is_bst_rec(int *a, int size, int i, int max, int min) {
-    if (i >= size)
-        return 1;
-    if (a[i] == NULL)
-        return 1;
-    if (a[i] < min || a[i] > max) {
-        return 0;
-    }
-    return is_bst_rec(a, size, 2*i+1, a[i]-1, min) && is_bst_rec(a, size, 2*i+2, max, a[i]+1);
+#define MAX_LINE_SIZE 10000   // maximum size of a line of input
+
+int scanArray(int *a) {
+    // scan line of text
+    char line[MAX_LINE_SIZE];
+    scanf("%[^\n]", line);
+    getchar();
+
+    // convert text into array
+    int size = 0, offset = 0, numFilled, n;
+    do {
+        numFilled = sscanf(line + offset, "%d%n", &(a[size]), &n);
+        if (numFilled > 0) {
+            size++;
+            offset += n;
+        } else if (a[size] == NULL ) {
+            size++;
+        }
+    } while (numFilled != EOF);
+
+    return size;
 }
 
-int is_bst(int *a, int size) {
-    return is_bst_rec(a, size, 0, INT_MAX, INT_MIN);
+int in_order(int *array, int size, int pos) {
+    if (array[pos] == -1)
+        return pos+1;
+    int k = in_order(array, size, pos+1);
+    printf("%d ", array[pos]);
+    in_order(array, size, k);
 }
+
+
 
 int main() {
-    printf("Hello, World!\n");
-    return is_bst();
+    int *a = calloc(MAX_LINE_SIZE, sizeof(int));
+    int size = scanArray(a);
+    a = realloc(a, size*sizeof(int));
+    in_order(a, size, 0);
+    return 0;
 }
